2f9392e85237ff9505e98b509042ba8a9dc62c6e
Andres Minder <andres.minder@students.fhnw.ch>
2018-04-17 16:00:59 +0200

    ...

--- BluetoothConfig/BluetoothConfig.ino
+++ BluetoothConfig/BluetoothConfig.ino
@@ -1,21 +1,22 @@
+#include <Arduino.h>
 #include <SoftwareSerial.h>
 
 SoftwareSerial BTSerial(10, 11); // RX | TX
 
 void setup() 
 {
-  Serial.begin(38400);
-  Serial.println("Enter AT commands:");
+  pinMode(9, OUTPUT);  digitalWrite(9, HIGH); 
+  Serial.begin(9600);
+  Serial.println("Enter AT-Commands:");
   BTSerial.begin(38400);  // HC-05 default speed in AT command more
-  pinMode(9, OUTPUT);  // this pin will pull the HC-05 pin 34 (key pin) HIGH to switch module to AT mode
-  digitalWrite(9, HIGH); 
   BTSerial.println("AT");
+  /*
   BTSerial.println("AT+NAME=Data_Analyzer");
   BTSerial.println("AT+NAME?");
   BTSerial.println("AT+PSWD=2017");
   BTSerial.println("AT+PSWD?");
   BTSerial.println("AT+UART=9600,0,0");
-  BTSerial.println("AT+UART?");
+  BTSerial.println("AT+UART?");*/
 }
 
 void loop()
