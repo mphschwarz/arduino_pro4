83eb6d71290aecf771cb8b991a045a465149447e
Andres Minder <andres.minder@students.fhnw.ch>
2018-04-21 14:49:44 +0200

    BT config files

--- BluetoothConfig/BluetoothConfig.ino
+++ BluetoothConfig/BluetoothConfig.ino
@@ -1,24 +1,56 @@
-#include <SoftwareSerial.h>
-
-SoftwareSerial BTSerial (8,9); //RXD | TXD
-
-void setup()
+//  SerialIn_SerialOut_HM-10_01
+//
+//  Uses hardware serial to talk to the host computer and AltSoftSerial for communication with the bluetooth module
+//
+//  What ever is entered in the serial monitor is sent to the connected device
+//  Anything received from the connected device is copied to the serial monitor
+//  Does not send line endings to the HM-10
+//
+//  Pins
+//  BT VCC to Arduino 5V out. 
+//  BT GND to GND
+//  Arduino D8 (SS RX) - BT TX no need voltage divider 
+//  Arduino D9 (SS TX) - BT RX through a voltage divider (5v to 3.3v)
+//
+ 
+#include <AltSoftSerial.h>
+AltSoftSerial BTSerial; 
+ 
+char c = ' ';
+boolean NL = true;
+ 
+void setup() 
 {
-  BTSerial.begin(9600);
-  Serial.begin(9600);
-  delay(100);
-  Serial.println("Enter AT-Commands:");
+    Serial.begin(9600);
+    Serial.println("Sketch:   BluetoothConfig");
+    Serial.print("Uploaded: ");   Serial.println(__DATE__);
+    BTSerial.begin(9600);  
+    Serial.println("BTserial started at 9600");
+    Serial.println(" ");
+    Serial.println("Enter AT-Commands:");
 }
-
+ 
 void loop()
 {
-  if(Serial.available() > 0)          
-  {
-    BTSerial.write(Serial.read());    //writes from arduino (serial monitor) to HM-10
-  }
-  if(BTSerial.available() > 0)        
-  {
-    Serial.write(BTSerial.read());    //writes response from HM-10 to arduino (serial monitor)
-  }
+    // Read from the Bluetooth module and send to the Arduino Serial Monitor
+    if (BTSerial.available())
+    {
+        c = BTSerial.read();
+        Serial.write(c);
+    }
+    // Read from the Serial Monitor and send to the Bluetooth module
+    if (Serial.available())
+    {
+        c = Serial.read();
+        // do not send line end characters to the HM-10
+        if (c != 10 & c != 13 ) 
+        {  
+             BTSerial.write(c);
+        }
+        // Echo the user input to the main window. 
+        // If there is a new line print the ">" character.
+        if (NL) { Serial.print("\r\n>");  NL = false; }
+        Serial.write(c);
+        if (c == 10) { NL = true; }
+    }
 }
-
