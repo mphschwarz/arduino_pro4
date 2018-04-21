977c9bfc60fede461906a279e22ada7c0e465b4f
Andres Minder <andres.minder@students.fhnw.ch>
2018-04-21 16:35:32 +0200

    Unnecessary

--- BluetoothConfig/BluetoothConfig.ino
+++ BluetoothConfig/BluetoothConfig.ino
@@ -14,41 +14,43 @@
 //
  
 #include <AltSoftSerial.h>
-AltSoftSerial BTSerial; 
+AltSoftSerial BTserial; 
  
-char c = ' ';
+ 
+char c=' ';
 boolean NL = true;
  
 void setup() 
 {
     Serial.begin(9600);
-    Serial.println("Sketch:   BluetoothConfig");
+    Serial.print("Sketch:   ");
+    Serial.println(" ");
     Serial.print("Uploaded: ");   Serial.println(__DATE__);
-    BTSerial.begin(9600);  
+    Serial.println(" ");
+ 
+    BTserial.begin(9600);  
     Serial.println("BTserial started at 9600");
     Serial.println(" ");
-    Serial.println("Enter AT-Commands:");
+    Serial.println("Enter AT Commands:");
 }
  
 void loop()
 {
     // Read from the Bluetooth module and send to the Arduino Serial Monitor
-    if (BTSerial.available())
+    if (BTserial.available())
     {
-        c = BTSerial.read();
+        c = BTserial.read();
         Serial.write(c);
     }
     // Read from the Serial Monitor and send to the Bluetooth module
     if (Serial.available())
     {
         c = Serial.read();
-        // do not send line end characters to the HM-10
-        if (c != 10 & c != 13 ) 
+        if (c!=10 & c!=13 ) 
         {  
-             BTSerial.write(c);
+             BTserial.write(c);
         }
-        // Echo the user input to the main window. 
-        // If there is a new line print the ">" character.
+        // Echo the user input to the main window. The ">" character indicates the user entered text.
         if (NL) { Serial.print("\r\n>");  NL = false; }
         Serial.write(c);
         if (c == 10) { NL = true; }
