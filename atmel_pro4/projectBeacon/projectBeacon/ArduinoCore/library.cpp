becd35c3821e689f8c9e93dc17e99130bc986ba8
Andres Minder <andres.minder@students.fhnw.ch>
2018-04-24 11:53:43 +0200

    in Atmel

---
+++ atmel_pro4/projectBeacon/projectBeacon/ArduinoCore/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 24.04.2018 09:12:24
+ * Author : andre
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
