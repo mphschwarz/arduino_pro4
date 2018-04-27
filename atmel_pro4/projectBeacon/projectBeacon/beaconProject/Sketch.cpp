bf4c9408f4b1a41a772c97e417173e9575a8e20f
Andres Minder <andres.minder@students.fhnw.ch>
2018-04-27 16:26:24 +0200

    atmel file
    scan for closest beacon works with vibrocontroll and a multiplexercontroll

--- atmel_pro4/projectBeacon/projectBeacon/beaconProject/Sketch.cpp
+++ atmel_pro4/projectBeacon/projectBeacon/beaconProject/Sketch.cpp
@@ -6,38 +6,52 @@
 
 SoftwareSerial BTSerial(2,3); //(RX | TX) - PINS
 
-#define VIBRO PORTC
-#define VIBRO_ON (1<<5)
 
+
+#define MULTIPLEXER_PORT	PORTC
+#define SD_PATH				0b00011010		//just default!!
+#define MC_PATH				0b00000101
+#define VIBRO_ON_OFF		0b00100000
+#define UUID_ARRAY_LENGTH	3
+#define RSSI_ARRAY_LENGTH	3
+#define MAJOR_ARRAY_LENGTH	4
+#define FILTER_MAJOR_VALUE	3				//to filter other beacons
+#define RSSI_STRENGTH		90				//to filter beacon with a too big distance
+
+unsigned int scanClosestBeacon();
 void scan();
 void sendCommand(int com);
 int readAnswer();
-int saveBeacon(int i);
+// int saveBeacon(int i);
 void filt(int answerByte);
 void rssiCompare();
 void firstResponse();
-int saveBeacon(int i);
+void compareBeacon();
+void multiplexController(int multiplex_path);
+void vibroController(boolean state);
+unsigned int readValueDec(int array_length);
+unsigned int castHexChararrToIntDec(char* array);
+void testComparing(unsigned int test);
 
-struct UUID_RSSI_values
+boolean firstBeacon = false;						//at first there must be a beacon for comparing to
+// unsigned int countBeacon = 1;
+
+struct BEACON_VALUES_DEC
 {
-	char UUID[32];
-	char value_RSSI[3];
+	unsigned int UUID_value;
+	unsigned int RSSI_value;			//its the absolut value of RSSI
+	unsigned int MAJOR_value;	
 };
 
-struct UUID_RSSI_values getUUID_RSSI();
-void printBeacons(UUID_RSSI_values beacon);
+BEACON_VALUES_DEC closest_Beacon;
 
-int incomingByte = 0;
+struct BEACON_VALUES_DEC getUUID_RSSI();
+void printBeacons(BEACON_VALUES_DEC beacon);
+
 char command_AT[2] = {'A','T'};
 char command_ATDISI[8] = {'A','T','+','D','I','S','I','?'};
 char endString[8];
 
-UUID_RSSI_values beacon1;
-UUID_RSSI_values beacon2;
-UUID_RSSI_values beacon3;
-UUID_RSSI_values beacon4;
-UUID_RSSI_values beacon5;
-UUID_RSSI_values beacon6;
 
 void setup() 
 {
@@ -79,17 +93,17 @@ void firstResponse()
 	}		
 }
 
-void scan()
+unsigned int scanClosestBeacon()
 {
-	int countBeacon = 1;
+	int compare_koeff;
+	
 	sendCommand(2);
-	int mischa_koeffizient = 0;
 	
 	while(1)
 	{
-		mischa_koeffizient = readAnswer();
-			
-		if(mischa_koeffizient == 58)
+		compare_koeff = readAnswer();
+		
+		if(compare_koeff == 58)
 		{
 			boolean state_zero = true;
        
@@ -103,7 +117,7 @@ void scan()
 			if(state_zero == false)                             
 			{
 				state_zero = true;
-				countBeacon = saveBeacon(countBeacon);
+				compareBeacon();
 				continue;
 			}
 			else
@@ -116,11 +130,14 @@ void scan()
 				continue;
 			}
 		}
-		else if (mischa_koeffizient == 69)
+		else if (compare_koeff == 69)
 		{
+// 			countBeacon = 1;
 			break;
 		}
 	}
+	firstBeacon = false;
+	return closest_Beacon.UUID_value;
 }
 
 void sendCommand(int com)
@@ -147,11 +164,14 @@ void sendCommand(int com)
 
 int readAnswer()
 {
-  while(!BTSerial.available())
-  {/*wait until datas are available to read*/}  
-  incomingByte = BTSerial.read();
+	int incomingByte;
+	
+	while(!BTSerial.available())
+	{/*wait until datas are available to read*/}  
+	  
+	incomingByte = BTSerial.read();
   
-  return incomingByte;
+	return incomingByte;
 }
 
 void filt(int answerByte)
@@ -159,74 +179,66 @@ void filt(int answerByte)
   
 }
 
-int saveBeacon(int i)
+void compareBeacon()
 {
-	switch(i)
+	BEACON_VALUES_DEC buff = getUUID_RSSI();
+	
+	if (buff.MAJOR_value == FILTER_MAJOR_VALUE && buff.RSSI_value <= RSSI_STRENGTH && firstBeacon == false)
 	{
-		case 1:
-			beacon1 = getUUID_RSSI();
-			printBeacons(beacon1);
-			break;
-		case 2:
-			beacon2 = getUUID_RSSI();
-			printBeacons(beacon2);
-			break;
-		case 3:
-			beacon3 = getUUID_RSSI();
-			printBeacons(beacon3);
-			break;
-		case 4:
-			beacon4 = getUUID_RSSI();
-			printBeacons(beacon4);
-			break;
-		case 5:
-			beacon5 = getUUID_RSSI();
-			printBeacons(beacon5);
-			break;
-		case 6:
-			beacon6 = getUUID_RSSI();
-			printBeacons(beacon6);
-			break;
-		default:
-			//do nothing;
-			break;
+		firstBeacon = true;
+		closest_Beacon = buff;
 	}
-	return i++;
+	else if (buff.MAJOR_value == FILTER_MAJOR_VALUE && buff.RSSI_value <= RSSI_STRENGTH && closest_Beacon.RSSI_value > buff.RSSI_value && firstBeacon == true)
+	{
+		closest_Beacon = buff;
+	}
 }
 
-struct UUID_RSSI_values getUUID_RSSI()
+struct BEACON_VALUES_DEC getUUID_RSSI()
 {
-	UUID_RSSI_values beacon;
+	BEACON_VALUES_DEC beacon;
 	
-	while(readAnswer() != 58)
+	while(readAnswer() != ':')
 	{/*wait until ':' is recived*/}
-	for(int i = 0; i < 32; i++)
-	{
-		beacon.UUID[i] = readAnswer();
-	}
-	while(readAnswer() != 45)
+	beacon.UUID_value = readValueDec(UUID_ARRAY_LENGTH);
+	
+	while (readAnswer() != ':')
+	{/*wait until ":" is recieved*/}
+	beacon.MAJOR_value = readValueDec(MAJOR_ARRAY_LENGTH);
+	
+	while(readAnswer() != '-')
 	{/*wait until '-' is recived*/}
-	for(int i = 0; i < 3; i++)
-	{
-		beacon.value_RSSI[i] = readAnswer();
-	}
+	beacon.RSSI_value = readValueDec(RSSI_ARRAY_LENGTH);
+			
 	return beacon;
 }
 
-void printBeacons(UUID_RSSI_values beacon)
+unsigned int readValueDec(int array_length)
+{	
+	unsigned int value = 0;
+	
+	for(int i = 0; i < array_length; i++)
+	{
+		value = (value * 10) + (readAnswer() - '0');
+	}
+	
+	return value;
+}
+
+void printBeacons(BEACON_VALUES_DEC beacon)
 {	
 	Serial.println("Beacon:");
 	Serial.print("UUID: ");
-	for(int i = 0; i < 32; i++)				//write values to emulator
-	{
-		Serial.write(beacon.UUID[i]);
-	}
+	Serial.print(beacon.UUID_value);
 	Serial.println("");
-	Serial.print("RSSI: "); Serial.print("-");
-	for(int i = 0; i < 3; i++)
-	{
-		Serial.write(beacon.value_RSSI[i]);			//end write values to emulator
-	}
+	
+	Serial.print("MAJOR: ");
+	Serial.print(beacon.MAJOR_value);
+	Serial.println("");
+	
+	Serial.print("RSSI: "); 
+	Serial.print("-");
+	Serial.print(beacon.RSSI_value);
 	Serial.println("dbm");
 }
 
@@ -235,10 +247,59 @@ void rssiCompare()
   
 }
 
+void multiplexController(int multiplex_path)
+{
+	MULTIPLEXER_PORT &= 0;			//set all to 0;
+	MULTIPLEXER_PORT |= multiplex_path;
+}
+
+void vibroController(boolean state)
+{
+	if (state == true)					//turns the vibro ON
+	{
+		MULTIPLEXER_PORT |= VIBRO_ON_OFF;
+		delay(500);
+		vibroController(false);
+	}
+	else if(state == false)				//turns the vibro OFF
+	{
+		MULTIPLEXER_PORT ^= VIBRO_ON_OFF;
+	}
+}
+
+unsigned int castHexChararrToIntDec(char* array)
+{
+	char* pEnd;
+	unsigned int cast = strtol(array, &pEnd ,16);
+	return cast;
+}
+
+void testComparing(unsigned int test)
+{
+	static unsigned int compare = 0;
+	
+	if (test != compare && test != 0)
+	{
+		compare = test;
+		Serial.println(compare);					//to print first three numbers of UUID
+		closest_Beacon.UUID_value = 0;
+		closest_Beacon.MAJOR_value = 0;
+		closest_Beacon.RSSI_value = 0;
+		vibroController(true);
+	}
+	else 
+	{
+		//do nothing!
+	}
+}
+
+void scan()
+{
+	testComparing(scanClosestBeacon());
+}
+
 void loop()
 {
-	Serial.println("Start scanning...");
 	scan();
-	Serial.println("Finished scanning!");
-	delay(4000);
+	delay(2000);
 }
