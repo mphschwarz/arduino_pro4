ec0ef2c96b88d31b7f78133f9544d93c82132ea1
Samuel <samuel.wey@students.fhnw.ch>
2018-05-08 14:11:03 +0200

    Auto stash before merge of "master" and "origin/master"
    dojo_project

---
+++ dojo_project/dojo_project/ArduinoCore/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 08.05.2018 13:50:12
+ * Author : Samuel Wey
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
