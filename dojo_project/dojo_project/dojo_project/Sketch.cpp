d44b5b603a2b609bcb47fd67e23080d0680b95ff
Samuel <samuel.wey@students.fhnw.ch>
2018-05-08 14:11:03 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ dojo_project/dojo_project/dojo_project/Sketch.cpp
@@ -0,0 +1,18 @@
+﻿
+#include <Arduino.h>
+#include <SoftwareSerial.h>
+#define LEDPORT PORTD
+#define LEDDDR DDRD
+
+SoftwareSerial mySerial = SoftwareSerial(0,1);
+void setup() {
+  LEDDDR = 0xFF;
+}
+
+void loop() {
+// LED 
+LEDPORT = 0xFF;
+_delay_ms(500);
+LEDPORT = 0x00;
+_delay_ms(500);
+}
