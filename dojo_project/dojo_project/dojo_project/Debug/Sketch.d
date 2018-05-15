d7f034937b2e98fb55ac14c65edcf45cd8de46d5
Samuel <samuel.wey@students.fhnw.ch>
2018-05-15 20:38:43 +0200

    dojo_project

--- dojo_project/dojo_project/dojo_project/Debug/Sketch.d
+++ dojo_project/dojo_project/dojo_project/Debug/Sketch.d
@@ -35,7 +35,8 @@ Sketch.d Sketch.o: .././Sketch.cpp \
  ..\\..\ArduinoCore\include\core/Arduino.h \
  ..\\..\ArduinoCore\include\variants\standard/pins_arduino.h \
  ..\include\libraries\SoftwareSerial\src/SoftwareSerial.h \
- ..\\..\ArduinoCore\include\core/Stream.h
+ ..\\..\ArduinoCore\include\core/Stream.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\time.h
 
 ..\\..\ArduinoCore\include\core/Arduino.h:
 
@@ -110,3 +111,5 @@ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\
 ..\include\libraries\SoftwareSerial\src/SoftwareSerial.h:
 
 ..\\..\ArduinoCore\include\core/Stream.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\time.h:
