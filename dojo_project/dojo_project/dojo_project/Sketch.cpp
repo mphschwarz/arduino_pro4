50538447d53b0fba1181208cff0a7f6fb2d118f5
Andres Minder <andres.minder@students.fhnw.ch>
2018-05-29 15:17:00 +0200

    BT

--- dojo_project/dojo_project/dojo_project/Sketch.cpp
+++ dojo_project/dojo_project/dojo_project/Sketch.cpp
@@ -352,59 +352,20 @@ void pinSetupWTV()
 
 void loop()
 {
+	sendCommand(1);
+	
+	
+	
 	if (BTSerial.available())
 	{
 		mySerial.write(readAnswer());
+		mySerial.write(readAnswer());
 	}
-	else
-	{
-		mySerial.write("a");
-	}
+	delay(500);
+
 // 	scan();
 // 	delay(2000);
 // 	mySerial.println("works!");
 // 	delay(1000);
-	firstResponse();
 // 	BTSerial.print("U");
-	delay(1000);
 }
-
-#define MULTIPLEXER_PORT	PORTC
-#define SD_PATH				0b00011010		//just default!!
-#define MC_PATH				0b00000101
-#define VIBRO_ON_OFF		0b00100000
-#define FTDI_PATH			0b00000000		//from MCU over FTDI to USB micro B to computer
-#define UUID_ARRAY_LENGTH	3
-#define RSSI_ARRAY_LENGTH	3
-#define MAJOR_ARRAY_LENGTH	4
-#define FILTER_MAJOR_VALUE	3				//to filter other beacons
-#define RSSI_STRENGTH		90				//to filter beacon with a too big distance
-
-#define WTV_DOUT			PORTB4
-#define WTV_CLK				PORTB5
-#define WTV_RESET			PORTB1
-
-#define OPCODE_PLAY_PAUSE	0xFFFE			//play track or stop the current, first send track number 0-511
-#define OPCODE_STOP			0xFFFF			//stops the current playback
-#define OPCODE_VOL			0xFFF0			//0xFFF0 is muted 0xFFF7 is max volume
-
-
-unsigned int scanClosestBeacon();
-void scan();
-void sendCommand(int com);
-int readAnswer();
-// int saveBeacon(int i);
-void filt(int answerByte);
-void rssiCompare();
-void firstResponse();
-void compareBeacon();
-void multiplexController(int multiplex_path);
-void vibroController(boolean state);
-unsigned int readValueDec(int array_length);
-unsigned int castHexChararrToIntDec(char* array);
-void testComparing(unsigned int test);
-void sendWTVcommand(unsigned int command);
-void resetWTV();
-void pinSetupWTV();
-
-	delay(500);	
